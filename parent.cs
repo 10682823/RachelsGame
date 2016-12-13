e42cfad65a9c85eedbac7ab046714b52e958064d
10682823 <burrows.rachel3@gmail.com>
2016-12-13 12:30:02 -0700

    Script-parent

---
+++ parent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parent : MonoBehaviour {
+
+	// Use this for initialization
+	protected void Start () {
+
+        myFunction();
+
+	
+	}
+
+    protected virtual void myFunction()
+
+    {
+        print("Parent Function Called");
+    }
+
+}
